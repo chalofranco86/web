b3af5881badaeca8ef057d5c87953e75caf7d9c1
chalofranco86 <121778441+chalofranco86@users.noreply.github.com>
2024-09-09 14:55:04 -0600

    Add files via upload

---
+++ database/migrations/2024_08_19_163525_create_historial_clinico_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('historial_clinico', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('paciente_id');
+            $table->unsignedBigInteger('medico_id');
+            $table->date('fecha');
+            $table->text('diagnostico');
+            $table->text('tratamiento');
+            $table->timestamps();
+
+            // Definir las llaves foráneas
+            $table->foreign('paciente_id')->references('id')->on('pacientes')->onDelete('cascade');
+            $table->foreign('medico_id')->references('id')->on('medicos')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('historial_clinico');
+    }
+};
